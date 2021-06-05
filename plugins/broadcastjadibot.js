4983867c304c4719fbf67f7b021c293c1425bac9
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-05 19:47:34 +0400

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, usedPrefix, text }) => {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Jadibot Broadcast 」')
+  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 TOPLU Avara BOT mesajı 」')
   for (let id of users) conn.copyNForward(id, content, true)
-  conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
+  conn.reply(m.chat, `_Avara BOT ${users.length} uğurla göndərildi_
 ${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu`).join('\n')}`.trim(), m)
 }
 handler.help = ['broadcastjadibot', 'bcbot'].map(v => v + ' <teks>')
