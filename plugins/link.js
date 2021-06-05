2384fcb18c796af697253b72ca54bdac65c6bafe
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-05 20:43:17 +0400

    Update link.js

--- plugins/link.js
+++ plugins/link.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, args }) => {
   let group = m.chat
   if (/^[0-9]{5,16}-[0-9]+@g\.us$/.test(args[0])) group = args[0]
-  if (!/^[0-9]{5,16}-[0-9]+@g\.us$/.test(group)) throw 'Hanya bisa dibuka di grup'
+  if (!/^[0-9]{5,16}-[0-9]+@g\.us$/.test(group)) throw 'Bu Komut Yalnız Grup Üçündür'
   let groupMetadata = await conn.groupMetadata(group)
-  if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-  if (!'participants' in groupMetadata) throw 'participants is not defined :('
+  if (!groupMetadata) throw 'qrup Metadata təyin edilməyib :\\'
+  if (!'participants' in groupMetadata) throw 'istifadəçilər müəyyən edilməyib :('
   let me = groupMetadata.participants.find(user => user.jid === conn.user.jid)
-  if (!me) throw 'Aku tidak ada di grup itu :('
-  if (me.isAdmin !== true) throw 'Aku bukan admin T_T'
+  if (!me) throw 'Mən o qrupda deyiləm :('
+  if (me.isAdmin !== true) throw 'Mən Admin Deyiləm T_T'
   m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
 }
 handler.help = ['linkgroup']
