604af0083071f212b3132af1ec0e8341eccab7c4
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-05 21:08:25 +0400

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -2,33 +2,33 @@ const { createHash } = require('crypto')
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.DATABASE._data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Format salah\n*${usedPrefix}daftar nama.umur*`
+  if (user.registered === true) throw `Zatən qeydiyyatdan keçmisiniz\nYeniden qeydiyyatdan keçmək istəyirsiz? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Yanlış format\n*${usedPrefix}qeydiyyat ad.yaş*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'Ad boş qala bilməz (Hərflər)'
+  if (!age) throw 'Yaş boş qala bilməz (Sayılar)'
   age = parseInt(age)
-  if (age > 120) throw 'Umur terlalu tua 😂'
-  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
+  if (age > 120) throw 'Yaş çox böyükdür 😂'
+  if (age < 5) throw 'Körpələr Qeydiyyatdan Keçirilmir ._.'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-Daftar berhasil!
+Qeydiyyat Uğurla Tamamlandı!
 
-╭─「 Info 」
-│ Nama: ${name}
-│ Umur: ${age} tahun
+╭─「 Məlumat 」
+│ Ad: ${name}
+│ Yaş: ${age} tahun
 │ SN: ${sn}
 ╰────
 `.trim())
 }
-handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
+handler.help = ['qeydiyyat', 'qy', 'register'].map(v => v + ' <ad>.<yaş>')
 handler.tags = ['exp']
 
-handler.command = /^(daftar|reg(ister)?)$/i
+handler.command = /^(qeydiyyat|qy(ister)?)$/i
 
 module.exports = handler
 
