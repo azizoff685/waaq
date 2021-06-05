b54a69cf0aeb0afdcb7755fe5d28be097e9eca48
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-05 20:27:26 +0400

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -2,7 +2,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text }) => {
     let [_, code] = text.match(linkRegex) || []
-    if (!code) throw 'Link invalid'
+    if (!code) throw 'Link yanlışdır'
     let res = await conn.acceptInvite(code)
     m.reply(`Uğurla qrupa qoşuldu ${res.gid}`)
 }
