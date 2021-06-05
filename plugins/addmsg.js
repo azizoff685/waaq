5817f3ea3b8409cb2176f71abb74b8f140fbc84d
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-05 19:23:00 +0400

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -2,12 +2,12 @@ let { WAMessageProto } = require('@adiwajshing/baileys')
 let handler = async (m, { command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
     let which = command.replace(/add/i, '')
-    if (!m.quoted) throw 'Reply Pesan!'
-    if (!text) throw `Gunakan *${usedPrefix}list${which}* untuk melihat list nya`
+    if (!m.quoted) throw 'Mesaj yanıtla!'
+    if (!text) throw `Siyahını görmək üçün *${usedPrefix}list${which}* yazın`
     let msgs = global.DATABASE._data.msgs
-    if (text in msgs) throw `'${text}' telah terdaftar di list pesan`
+    if (text in msgs) mesaj siyahısında qeydiyyatdan keçib`'${text}' atmaq`
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
-    m.reply(`Berhasil menambahkan pesan di list pesan sebagai '${text}'
+    m.reply(`Mesaj siyahısına mesaj uğurla əlavə edildi '${text}'
     
 Akses dengan ${usedPrefix}get${which} ${text}`)
 }
