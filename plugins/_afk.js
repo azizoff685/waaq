e682cd478d478d3003860a41d9febfcd2cbac9fe
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-05 19:02:21 +0400

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -3,8 +3,8 @@ handler.before = m => {
   let user = global.DATABASE.data.users[m.sender]
   if (user.afk > -1) {
     m.reply(`
-Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
-Selama ${clockString(new Date - user.afk)}
+Kamu berhenti AFK${user.afkReason ? ' sonra ' + user.afkReason : ''}
+Sırasında ${clockString(new Date - user.afk)}
 `.trim())
     user.afk = -1
     user.afkReason = ''
@@ -17,9 +17,9 @@ Selama ${clockString(new Date - user.afk)}
     if (!afkTime || afkTime < 0) continue
     let reason = user.afkReason || ''
     m.reply(`
-Jangan tag dia!
-Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
-Selama ${clockString(new Date - afkTime)}
+Onu etiketləməyin!
+Dia sedang AFK ${reason ? 'səbəblə ' + reason : 'səbəbsiz'}
+Ərzində ${clockString(new Date - afkTime)}
 `.trim())
   }
   return true
