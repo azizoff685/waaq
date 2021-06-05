925ba5802db5f88b214eba9314da6a500f04d265
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-05 20:45:14 +0400

    Update listprem.js

--- plugins/listprem.js
+++ plugins/listprem.js
@@ -1,14 +1,14 @@
 /*
    Silahkan Di Pakek
    Tapi Bantu Rapihin :v
-   Buatan: Miaweers
+   Buatan: Azizoff
 */
 
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
   let prem = global.prems.map(v => v.replace(/[^0-9]/g, '')  + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-conn.sendMessage(m.chat, `「 List Premium 」` + `\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, MessageType.extendedText, { contextInfo: { mentionedJid: prem } })
+conn.sendMessage(m.chat, `「 Premium Siyahısı 」` + `\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, MessageType.extendedText, { contextInfo: { mentionedJid: prem } })
 }
 handler.command = /^(listprem)$/i
 handler.owner = true
