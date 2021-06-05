502fb1729f8ab47451e75f6758fbff9e2feb53cf
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-05 20:30:37 +0400

    Update leaderboard.js

--- plugins/leaderboard.js
+++ plugins/leaderboard.js
@@ -11,18 +11,18 @@ let handler = async (m, { conn, args, participants }) => {
   console.log(participants)
   let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
   let text = `
-• *XP Leaderboard Top ${len}* •
-Kamu: *${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
+• *XP Lideri Top ${len}* •
+Sən: *${usersExp.indexOf(m.sender) + 1}* dan *${usersExp.length}*
 
 ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} Exp*`).join`\n`}
 
-• *Limit Leaderboard Top ${len}* •
-Kamu: *${usersLim.indexOf(m.sender) + 1}* dari *${usersLim.length}*
+• *Limit Lideri Top ${len}* •
+Sən: *${usersLim.indexOf(m.sender) + 1}* dan *${usersLim.length}*
 
 ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} Limit*`).join`\n`}
 
-• *Level Leaderboard Top ${len}* •
-Kamu: *${usersLevel.indexOf(m.sender) + 1}* dari *${usersLevel.length}*
+• *Level Lideri Top ${len}* •
+Sən: *${usersLevel.indexOf(m.sender) + 1}* dan *${usersLevel.length}*
 
 ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Level ${level}*`).join`\n`}
 `.trim()
