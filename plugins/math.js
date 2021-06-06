44d6b6261cd44205066b8164e1a4ae8c70104bd5
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-06 16:47:54 +0400

    Update math.js

--- plugins/math.js
+++ plugins/math.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn, args, usedPrefix }) => {
   if (args.length < 1) throw `
 Mode: ${Object.keys(modes).join(' | ')}
 
-İstifadəyə dair nümunələr: ${usedPrefix}math medium
+İstifadəyə dair nümunələr: ${usedPrefix}math orta
 `.trim()
   let mode = args[0].toLowerCase()
   if (!(mode in modes)) throw `
 Mode: ${Object.keys(modes).join(' | ')}
 
-İstifadəyə dair nümunələr: ${usedPrefix}math medium
+İstifadəyə dair nümunələr: ${usedPrefix}math orta
 `.trim()
   let id = m.chat
   if (id in conn.math) return conn.reply(m.chat, 'Bu söhbətdə hələ də cavabsız suallar var', conn.math[id][0])
@@ -30,13 +30,13 @@ handler.command = /^math/i
 module.exports = handler
 
 let modes = {
-  noob: [-3, 3,-3, 3, '+-', 15000, 10],
-  easy: [-10, 10, -10, 10, '*/+-', 20000, 40],
-  medium: [-40, 40, -20, 20, '*/+-', 40000, 150],
-  hard: [-100, 100, -70, 70, '*/+-', 60000, 350],
-  extreme: [-999999, 999999, -999999, 999999, '*/', 99999, 9999],
-  impossible: [-99999999999, 99999999999, -99999999999, 999999999999, '*/', 30000, 35000],
-  impossible2: [-999999999999999, 999999999999999, -999, 999, '/', 30000, 50000]
+  çoxasan: [-3, 3,-3, 3, '+-', 15000, 10],
+  asan: [-10, 10, -10, 10, '*/+-', 20000, 40],
+  orta: [-40, 40, -20, 20, '*/+-', 40000, 150],
+  çətin: [-100, 100, -70, 70, '*/+-', 60000, 350],
+  çoxçətin: [-999999, 999999, -999999, 999999, '*/', 99999, 9999],
+  dəlilik: [-99999999999, 99999999999, -99999999999, 999999999999, '*/', 30000, 35000],
+  mümkünatsız: [-999999999999999, 999999999999999, -999, 999, '/', 30000, 50000]
 } 
 
 let operators = {
