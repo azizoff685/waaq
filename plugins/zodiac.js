424f3f91bd8d26bc4f939f63fb8201e0593d43dc
Nihad <82688067+azizoff685@users.noreply.github.com>
2021-06-09 04:47:21 +0400

    Update zodiac.js

--- plugins/zodiac.js
+++ plugins/zodiac.js
@@ -15,34 +15,34 @@ let handler = (m, { usedPrefix, command, text }) => {
     const cekusia = bulan === birth[1] && tanggal === birth[2] ? `Selamat ulang tahun yang ke-${age} 🥳` : age
 
     const teks = `
-Lahir : ${birth.join('-')}
-Ultah Mendatang : ${birthday.join('-')}
-Usia : ${cekusia}
-Zodiak : ${zodiac}
+Anadan Olma : ${birth.join('-')}
+Gələn Ad Günü : ${birthday.join('-')}
+Yaş : ${cekusia}
+Bürc : ${zodiac}
 `.trim()
     m.reply(teks)
 }
-handler.help = ['zodiac *2002 02 25*']
+handler.help = ['burc *2002 02 25*']
 handler.tags = ['tools']
 
-handler.command = /^zodia[kc]$/i
+handler.command = /^burc[kc]$/i
 
 module.exports = handler
 
 const zodiak = [
-    ["Capricorn", new Date(1970, 0, 1)],
-    ["Aquarius", new Date(1970, 0, 20)],
-    ["Pisces", new Date(1970, 1, 19)],
-    ["Aries", new Date(1970, 2, 21)],
-    ["Taurus", new Date(1970, 3, 21)],
-    ["Gemini", new Date(1970, 4, 21)],
-    ["Cancer", new Date(1970, 5, 22)],
-    ["Leo", new Date(1970, 6, 23)],
-    ["Virgo", new Date(1970, 7, 23)],
-    ["Libra", new Date(1970, 8, 23)],
-    ["Scorpio", new Date(1970, 9, 23)],
-    ["Sagittarius", new Date(1970, 10, 22)],
-    ["Capricorn", new Date(1970, 11, 22)]
+    ["Oğlaq", new Date(1970, 0, 1)],
+    ["Dolça", new Date(1970, 0, 20)],
+    ["Balıq", new Date(1970, 1, 19)],
+    ["Qoç", new Date(1970, 2, 21)],
+    ["Buğa", new Date(1970, 3, 21)],
+    ["Əkizlər", new Date(1970, 4, 21)],
+    ["Xərçəng", new Date(1970, 5, 22)],
+    ["Şir", new Date(1970, 6, 23)],
+    ["Qız", new Date(1970, 7, 23)],
+    ["Tərəzi", new Date(1970, 8, 23)],
+    ["Əqrəb", new Date(1970, 9, 23)],
+    ["Oxatan", new Date(1970, 10, 22)],
+    ["Oğlaq", new Date(1970, 11, 22)]
 ].reverse()
 
 function getZodiac(month, day) {
